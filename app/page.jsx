3c1931cc05962cef1a535de6273903587b9c71ca
harvs05 <harveyv291@gmail.com>
2026-01-05 20:32:11 +0800

    new updated

--- app/page.jsx
+++ app/page.jsx
@@ -27,7 +27,7 @@ const Home = () => {
                 {/* button and socials */}
                 <div className="flex flex-col xl:flex-row items-center gap-8">
                     <a 
-                        href="/harveyvarelaresume.pdf" 
+                        href="/harveyvarelaresumeupdated.pdf" 
                         download 
                         className="uppercase"
                     >
